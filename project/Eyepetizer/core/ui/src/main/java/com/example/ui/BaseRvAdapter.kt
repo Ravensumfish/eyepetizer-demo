a80a0b46eb140b792bba57aec6f4120d9c22e590
Ravensumfish <3100776336@qq.com>
2026-07-15 12:10:36 +0800

    feat:创建ui模块rv适配器基类

---
+++ project/Eyepetizer/core/ui/src/main/java/com/example/ui/BaseRvAdapter.kt
@@ -0,0 +1,70 @@
+/**
+ * description: 基本的Rv适配器
+ * author:Manticore
+ * email:3100776336@qq.com
+ * date:2026/7/15
+ */
+
+package com.example.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+//传入数据类型，rv布局 R.layout.xxx
+open class BaseRvAdapter<T>(
+    private val layoutResId : Int,
+    private val onItemClick : ((position : Int,item:View)-> Unit)? = null)
+    : RecyclerView.Adapter<BaseRvAdapter<T>.BaseRvViewHolder>(){
+
+    private val data = mutableListOf<T>()
+
+    //用于提交更新后的rv列表
+    //使用 submitList 传入数据列表才能刷新rv
+    fun submitList(list : List<T>){
+        data.clear()
+        data.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BaseRvViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(layoutResId,parent,false)
+        return BaseRvViewHolder(view)
+
+    }
+
+    //子类去实现具体绑定逻辑
+    //如
+    //holder.content.text = item.content (对应控件id)
+    override fun onBindViewHolder(holder: BaseRvViewHolder, position: Int) {
+        val item = data[position]
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    //子类去实现具体绑定逻辑
+ inner class BaseRvViewHolder(item: View): RecyclerView.ViewHolder(item){
+
+        init {
+            item.setOnClickListener {
+                val pos = adapterPosition
+                if (pos != RecyclerView.NO_POSITION){
+                    onItemClick?.invoke(pos, item)
+                }
+            }
+        }
+    }
+    /*如
+    val content : TextView = item.findViewById(R.id.xxx)
+    //...
+    * */
+
+}
